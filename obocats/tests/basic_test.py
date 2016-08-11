9f38dc86a8dba9a8c17958567e2d11a422c4422d
Eugene Hinderer <ehinderer01@gmail.com>
2016-08-11 15:00:21 -0400

    Final testing before merging with master.

--- obocats/tests/basic_test.py
+++ obocats/tests/basic_test.py
@@ -5,31 +5,31 @@ from . import godag
 from . import subdag
 
 mlab_database = open('/mlab/data/databases/GeneOntology/05-26-2016/go.obo', 'r')  # @ work
-home_database = open('/home/eugene/Databases/GeneOntology/06-14-2016/go.obo', 'r')  # @home
+#home_database = open('/home/eugene/Databases/GeneOntology/06-14-2016/go.obo', 'r')  # @home
 
 # Parse GO and make the graph. 
 go_dag = godag.GoGraph()
 go_parser = parser.GoParser(mlab_database, go_dag)
 go_parser.parse()
-database.close()
+mlab_database.close()
 go_dag.connect_nodes()
 
 # Test printing
-#print(len(go_dag.node_list))
-#print(len(go_dag.edge_list))
-#for node in go_dag.node_list:
-#    print(node.id)
+print(len(go_dag.node_list))
+print(len(go_dag.edge_list))
+for node in go_dag.node_list:
+    print(node.id)
 	
-#for edge in go_dag.edge_list:
-#    print(edge.child_id, '-->', edge.parent_id, edge.relationship)
+for edge in go_dag.edge_list:
+    print(edge.child_id, '-->', edge.parent_id, edge.relationship)
 
-#print(go_dag.used_relationship_set)
+print(go_dag.used_relationship_set)
 
 test_node = go_dag.node_list[1117]
 
-#print([node.id for node in test_node.parent_node_set], '\n', '    -->', test_node.id, '\n', '        ', [node.id for node in test_node.child_node_set])
-#for node in go_dag.root_nodes:
-#	print(node.name)
+print([node.id for node in test_node.parent_node_set], '\n', '    -->', test_node.id, '\n', '        ', [node.id for node in test_node.child_node_set])
+for node in go_dag.root_nodes:
+	print(node.name)
 
 """tests the path traversal method that doesnt work
 node1 = go_dag.id_index['GO:0051079']
