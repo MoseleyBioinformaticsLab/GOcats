978ce7e7607b36a2389512c37586e9e670336c45
Eugene Hinderer <ehinderer01@gmail.com>
2016-08-12 12:10:41 -0400

    Added if statement to ensure that the required 'is_a' relationship is specified in the allowed_relationships positional argument. Added a helpful warning message for incorrectly or invalid GO namespace filters. Added properties orphans and leaves to the OboGraph. Added methods to find all ancestors or all descendents of a given node. All appear to be working presently.

--- obocats/godag.py
+++ obocats/godag.py
@@ -6,10 +6,10 @@ class GoGraph(OboGraph):
     """A Gene-Ontology-specific graph. GO-specific idiosyncrasies go here."""
     
     def __init__(self, namespace_filter=None, allowed_relationships=None):
-        """This looks goofy as hell."""
-        self.valid_namespaces = ['cellular_component', 'biological_process', 'molecular_function']
+        self.valid_namespaces = ['cellular_component', 'biological_process', 'molecular_function', None]
         if namespace_filter not in self.valid_namespaces:
-            raise Exception("{} is not a valid Gene Ontology namespace.".format(namespace_filter))
+            raise Exception("{} is not a valid Gene Ontology namespace.\nPlease select from the following: {}".format(namespace_filter, self.valid_namespaces))
+
         super().__init__(namespace_filter=namespace_filter, allowed_relationships=allowed_relationships)
 
 
--- obocats/tests/basic_test.py
+++ obocats/tests/basic_test.py
@@ -8,27 +8,28 @@ from . import subdag
 home_database = open('/home/eugene/Databases/GeneOntology/06-14-2016/go.obo', 'r')  # @home
 
 # Parse GO and make the graph. 
-go_dag = godag.GoGraph(namespace_filter="biological_process")
+go_dag = godag.GoGraph()
 go_parser = parser.GoParser(home_database, go_dag)
 go_parser.parse()
 home_database.close()
 go_dag.connect_nodes()
 
 # Test printing
-print(len(go_dag.node_list))
-print(len(go_dag.edge_list))
-for node in go_dag.node_list:
-    print(node.id)
+#print(len(go_dag.node_list))
+#print(len(go_dag.edge_list))
+#for node in go_dag.node_list:
+#    print(node.id)
 	
-for edge in go_dag.edge_list:
-    print(edge.child_id, '-->', edge.parent_id, edge.relationship)
+#for edge in go_dag.edge_list:
+#    print(edge.child_id, '-->', edge.parent_id, edge.relationship)
 
 print(go_dag.used_relationship_set)
 
 test_node = go_dag.node_list[1117]
-
 print([node.id for node in test_node.parent_node_set], '\n', '    -->', test_node.id, '\n', '        ', [node.id for node in test_node.child_node_set])
-for node in go_dag.root_nodes:
+#for node in go_dag.root_nodes:
+#	print(node.name)
+for node in go_dag.descendants(test_node):
 	print(node.name)
 
 """tests the path traversal method that doesnt work
@@ -38,10 +39,9 @@ test_paths = go_dag.find_all_paths(node1, node2)
 print(test_paths)
 """
 
-subdag = subdag.SubGraph.from_filtered_graph(go_dag, ['mitochondrion', 'mitochondria', 'mitochondrial'])
-#for node in sub_dag.node_list:
-#	print(node)
-
-#print([node.name for node in sub_dag.allowed_nodes])
-#for node in subdag.node_list:
+#for node in go_dag.orphans:
 #	print(node.name)
+
+subdag = subdag.SubGraph.from_filtered_graph(go_dag, ['mitochondrion', 'mitochondria', 'mitochondrial'])
+#for node in subdag.node_list:
+#	print(node)
