c5ef7bf5f58094e5eb67c23d90af1849690a9940
Eugene Hinderer <ehinderer01@gmail.com>
2016-12-11 18:08:27 -0500

    added possible mappings without has_part test for all subontologies

--- obocats/hp_mapping_testing.py
+++ obocats/hp_mapping_testing.py
@@ -5,7 +5,9 @@ import itertools
 cc_graph = obocats.build_graph_interpreter('/mlab/data/databases/GeneOntology/01-12-2016/go.obo', supergraph_namespace='cellular_component')
 cc_no_hp_graph = obocats.build_graph_interpreter('/mlab/data/databases/GeneOntology/01-12-2016/go.obo', supergraph_namespace='cellular_component', allowed_relationships=['is_a', 'part_of'])
 mf_graph = obocats.build_graph_interpreter('/mlab/data/databases/GeneOntology/01-12-2016/go.obo', supergraph_namespace='molecular_function')
+mf_no_hp_graph = obocats.build_graph_interpreter('/mlab/data/databases/GeneOntology/01-12-2016/go.obo', supergraph_namespace='molecular_function', allowed_relationships=['is_a', 'part_of'])
 bp_graph = obocats.build_graph_interpreter('/mlab/data/databases/GeneOntology/01-12-2016/go.obo', supergraph_namespace='biological_process')
+bp_no_hp_graph = obocats.build_graph_interpreter('/mlab/data/databases/GeneOntology/01-12-2016/go.obo', supergraph_namespace='biological_process', allowed_relationships=['is_a', 'part_of'])
 
 cc_hp_edges = [edge for edge in cc_graph.edge_list if edge.relationship.id == 'has_part']
 mf_hp_edges = [edge for edge in mf_graph.edge_list if edge.relationship.id == 'has_part']
@@ -14,8 +16,9 @@ bp_hp_edges = [edge for edge in bp_graph.edge_list if edge.relationship.id == 'h
 all_cc_nodes = cc_graph.node_list
 all_cc_no_hp_nodes = cc_no_hp_graph.node_list
 all_mf_nodes = mf_graph.node_list
+all_mf_no_hp_nodes = mf_no_hp_graph.node_list
 all_bp_nodes = bp_graph.node_list
-
+all_bp_no_hp_nodes = bp_no_hp_graph.node_list
 
 def _potential_false_ancestors(edge):
     """Considering a problematic relationship edge, returns a set of nodes target that could result in a problematic
@@ -62,10 +65,12 @@ all_possible_cc_no_hp_mappings = all_possible_mappings(all_cc_no_hp_nodes)
 # molecular_function
 potential_false_mf_hp_mappings = potential_false_mappings(mf_hp_edges)
 all_possible_mf_mappings = all_possible_mappings(all_mf_nodes)
+all_possible_mf_no_hp_mappings = all_possible_mappings(all_mf_no_hp_nodes)  
 
 # biological_process
 potential_false_bp_hp_mappings = potential_false_mappings(bp_hp_edges)
 all_possible_bp_mappings = all_possible_mappings(all_bp_nodes)
+all_possible_bp_no_hp_mappings = all_possible_mappings(all_bp_no_hp_nodes)
 
 print("-----cellular_component-----")
 print("Potentially false 'has part' mappings: ", len(potential_false_cc_hp_mappings))
@@ -76,10 +81,12 @@ print("Intersection of possible true mappings and potentially false 'has_part' m
 print("-----molecular_function-----")
 print("Potentially false 'has part' mappings: ", len(potential_false_mf_hp_mappings))
 print("All possible mappings: ", len(all_possible_mf_mappings))
+print("All possible mappings withough 'has_part': ", len(all_possible_mf_no_hp_mappings))
 print("Intersection of possible true mappings and potentially false 'has_part' mappings: ", len(all_possible_mf_mappings.intersection(potential_false_mf_hp_mappings)))
 
 print("-----biological_process-----")
 print("Potentially false 'has part' mappings: ", len(potential_false_bp_hp_mappings))
 print("All possible mappings: ", len(all_possible_bp_mappings))
+print("All possible mappings without 'has_part': ", len(all_possible_bp_no_hp_mappings))
 print("Intersection of possible true mappings and potentially false 'has_part' mappings: ", len(all_possible_bp_mappings.intersection(potential_false_bp_hp_mappings)))
 
