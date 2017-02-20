4417d11af6ac477380b15959dcb065b543894a8b
Eugene Hinderer <ehinderer01@gmail.com>
2017-02-20 16:07:10 -0500

    Fixed erroranalysis.py to properly evaluate m2s mappings for subontologies. Forgot to limit tuples to only those with both positions in the subontology of interest.

--- gocats/tests/Map2SlimMappingTest/erroranalysis.py
+++ gocats/tests/Map2SlimMappingTest/erroranalysis.py
@@ -15,12 +15,13 @@ import gocats
 
 with open(sys.argv[2], 'r') as m2s_file:
     json_str = m2s_file.read()
-    m2s_mapping_tuples = jsonpickle.decode(json_str)
+    m2s_full_mapping_tuples = jsonpickle.decode(json_str)
 
-full_graph = gocats.build_graph_interpreter(sys.argv[3], allowed_relationships=['is_a','part_of','has_part'])
-cc_graph = gocats.build_graph_interpreter(sys.argv[3], supergraph_namespace='cellular_component', allowed_relationships=['is_a','part_of','has_part'])
-mf_graph = gocats.build_graph_interpreter(sys.argv[3], supergraph_namespace='molecular_function', allowed_relationships=['is_a','part_of','has_part'])
-bp_graph = gocats.build_graph_interpreter(sys.argv[3], supergraph_namespace='biological_process', allowed_relationships=['is_a','part_of','has_part'])
+full_graph = gocats.build_graph_interpreter(sys.argv[3], allowed_relationships=['is_a', 'part_of', 'has_part'])
+cc_graph = gocats.build_graph_interpreter(sys.argv[3], supergraph_namespace='cellular_component', allowed_relationships=['is_a', 'part_of', 'has_part'])
+mf_graph = gocats.build_graph_interpreter(sys.argv[3], supergraph_namespace='molecular_function', allowed_relationships=['is_a', 'part_of', 'has_part'])
+bp_graph = gocats.build_graph_interpreter(sys.argv[3], supergraph_namespace='biological_process', allowed_relationships=['is_a', 'part_of', 'has_part'])
+
 
 def all_possible_mappings(node_list):
     node_mapping_tuples = set()
@@ -30,25 +31,48 @@ def all_possible_mappings(node_list):
                 node_mapping_tuples.add((node1.id, node2.id))
     return node_mapping_tuples
 
+
 def erroneous_m2s_tuples(m2s_tuples, gc_tuples):
     return m2s_tuples - m2s_tuples.intersection(gc_tuples)
 
-full_mapping_tuples = all_possible_mappings(full_graph.node_list) 
-cc_mapping_tuples = all_possible_mappings(cc_graph.node_list)
-mf_mapping_tuples = all_possible_mappings(mf_graph.node_list)
-bp_mapping_tuples = all_possible_mappings(bp_graph.node_list)
+gc_full_mapping_tuples = all_possible_mappings(full_graph.node_list)
+gc_cc_mapping_tuples = all_possible_mappings(cc_graph.node_list)
+gc_mf_mapping_tuples = all_possible_mappings(mf_graph.node_list)
+gc_bp_mapping_tuples = all_possible_mappings(bp_graph.node_list)
 
-full_m2s_errors = erroneous_m2s_tuples(m2s_mapping_tuples, full_mapping_tuples)
-cc_m2s_errors = erroneous_m2s_tuples(m2s_mapping_tuples, cc_mapping_tuples)
-mf_m2s_errors = erroneous_m2s_tuples(m2s_mapping_tuples, mf_mapping_tuples)
-bp_m2s_errors = erroneous_m2s_tuples(m2s_mapping_tuples, bp_mapping_tuples)
+m2s_cc_mapping_tuples = set([tup for tup in m2s_full_mapping_tuples if (tup[0] and tup[1] in cc_graph.id_index.keys())])
+m2s_mf_mapping_tuples = set([tup for tup in m2s_full_mapping_tuples if (tup[0] and tup[1] in mf_graph.id_index.keys())])
+m2s_bp_mapping_tuples = set([tup for tup in m2s_full_mapping_tuples if (tup[0] and tup[1] in bp_graph.id_index.keys())])
 
-print("Full GO\n-------------------------\n# of mapping errors: {}\n% of erroneous mappings: {}\n# of correct mappings: {}\n# of gocats mappings: {}".format(len(full_m2s_errors), (len(full_m2s_errors)/len(full_mapping_tuples)), (len(m2s_mapping_tuples.intersection(full_mapping_tuples))), len(full_mapping_tuples)))
-print("# of m2s mappings for all GO: {}".format(len(m2s_mapping_tuples)))
+full_m2s_errors = erroneous_m2s_tuples(m2s_full_mapping_tuples, gc_full_mapping_tuples)
+cc_m2s_errors = erroneous_m2s_tuples(m2s_cc_mapping_tuples, gc_cc_mapping_tuples)
+mf_m2s_errors = erroneous_m2s_tuples(m2s_mf_mapping_tuples, gc_mf_mapping_tuples)
+bp_m2s_errors = erroneous_m2s_tuples(m2s_bp_mapping_tuples, gc_bp_mapping_tuples)
 
-print("CC ONLY\n-------------------------\n# of mapping errors: {}\n% of erroneous mappings: {}\n# of correct mappings: {}\n# of gocats mappings: {}".format(len(cc_m2s_errors), (len(cc_m2s_errors)/len(cc_mapping_tuples)), (len(m2s_mapping_tuples.intersection(cc_mapping_tuples))), len(cc_mapping_tuples)))
+print("Full GO\n-------------------------")
+print("Number of GOcats mappings: ", len(gc_full_mapping_tuples))
+print("Number of m2s mappings for all GO: ", (len(m2s_full_mapping_tuples)))
+print("Number of mapping errors: ", len(full_m2s_errors))
+print("Ratio of erroneous mappings to correct mappings in full GO: ", (len(full_m2s_errors)/len(gc_full_mapping_tuples)))
+print("Number of correct mappings determined by Map2Slim: ", (len(m2s_full_mapping_tuples.intersection(gc_full_mapping_tuples))))
 
-print("MF ONLY\n-------------------------\n# of mapping errors: {}\n% of erroneous mappings: {}\n# of correct mappings: {}\n# of gocats mappings: {}".format(len(mf_m2s_errors), (len(mf_m2s_errors)/len(mf_mapping_tuples)), (len(m2s_mapping_tuples.intersection(mf_mapping_tuples))), len(mf_mapping_tuples)))
+print("CC sub-ontology\n-------------------------")
+print("Number of GOcats mappings: ", len(gc_cc_mapping_tuples))
+print("Number of m2s mappings for CC: ", (len(m2s_cc_mapping_tuples)))
+print("Number of mapping errors: ", len(cc_m2s_errors))
+print("Ratio of erroneous mappings to correct mappings in CC: ", (len(cc_m2s_errors)/len(gc_cc_mapping_tuples)))
+print("Number of correct mappings determined by Map2Slim: ", (len(m2s_cc_mapping_tuples.intersection(gc_cc_mapping_tuples))))
 
-print("BP ONLY\n-------------------------\n# of mapping errors: {}\n% of erroneous mappings: {}\n# of correct mappings: {}\n# of gocats mappings: {}".format(len(bp_m2s_errors), (len(bp_m2s_errors)/len(bp_mapping_tuples)), (len(m2s_mapping_tuples.intersection(bp_mapping_tuples))), len(bp_mapping_tuples)))
+print("MF sub-ontology\n-------------------------")
+print("Number of GOcats mappings: ", len(gc_mf_mapping_tuples))
+print("Number of m2s mappings for MF: ", (len(m2s_mf_mapping_tuples)))
+print("Number of mapping errors: ", len(mf_m2s_errors))
+print("Ratio of erroneous mappings to correct mappings in MF: ", (len(mf_m2s_errors)/len(gc_mf_mapping_tuples)))
+print("Number of correct mappings determined by Map2Slim: ", (len(m2s_mf_mapping_tuples.intersection(gc_mf_mapping_tuples))))
 
+print("BP sub-ontology\n-------------------------")
+print("Number of GOcats mappings: ", len(gc_bp_mapping_tuples))
+print("Number of m2s mappings for BP: ", (len(m2s_bp_mapping_tuples)))
+print("Number of mapping errors: ", len(bp_m2s_errors))
+print("Ratio of erroneous mappings to correct mappings in BP: ", (len(bp_m2s_errors)/len(gc_bp_mapping_tuples)))
+print("Number of correct mappings determined by Map2Slim: ", (len(m2s_bp_mapping_tuples.intersection(gc_bp_mapping_tuples))))
