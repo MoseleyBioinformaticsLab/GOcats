24ea54653c33c1b02fa9d8b067376b785ebad12f
Eugene Hinderer <ehinderer01@gmail.com>
2017-02-20 23:18:46 -0500

    Attempted fix to incorrect count of all_possible_mappings in hp_mapping_testing.py, caused by accidentally altering objects while iterating over them. Re-evaluating error analysis of all term-to-term M2S mappings with respect to scoping. Now taking intersection of sub-ontology constrained m2s mapping pairs to ALL of GOcats' possible mappings across all GO. Calculating percent error of M2S as opposed to error ratio.

--- gocats/hp_mapping_testing.py
+++ gocats/hp_mapping_testing.py
@@ -25,20 +25,20 @@ all_bp_no_hp_nodes = bp_no_hp_graph.node_list
 def _potential_false_ancestors(edge):
     """Considering a problematic relationship edge, returns a set of nodes target that could result in a problematic
     mapping"""
-    child_ancestors = edge.child_node.ancestors
+    child_ancestors = set(edge.child_node.ancestors)
     child_ancestors.add(edge.child_node)
-    parent_ancestors = edge.parent_node.ancestors
+    parent_ancestors = set(edge.parent_node.ancestors)
     parent_ancestors.add(edge.parent_node)
 
     return child_ancestors - parent_ancestors
 
 
 def _potential_false_descendants(edge):
-    """Considering a problematic relationsihp edge, returns a set of source nodes that could result in a problematic
+    """Considering a problematic relationship edge, returns a set of source nodes that could result in a problematic
     mapping."""
-    parent_descendants = edge.parent_node.descendants
+    parent_descendants = set(edge.parent_node.descendants)
     parent_descendants.add(edge.parent_node)
-    child_descendants = edge.child_node.descendants
+    child_descendants = set(edge.child_node.descendants)
     child_descendants.add(edge.child_node)
 
     return parent_descendants - child_descendants
@@ -59,7 +59,7 @@ def all_possible_mappings(node_list):
                 node_mapping_tuples.add((node1, node2))
     return node_mapping_tuples
 
-# celluar_component
+# cellular_component
 potential_false_cc_hp_mappings = potential_false_mappings(cc_hp_edges)
 all_possible_cc_mappings = all_possible_mappings(all_cc_nodes)
 all_possible_cc_no_hp_mappings = all_possible_mappings(all_cc_no_hp_nodes)
@@ -83,7 +83,7 @@ print("Intersection of possible true mappings and potentially false 'has_part' m
 print("-----molecular_function-----")
 print("Potentially false 'has part' mappings: ", len(potential_false_mf_hp_mappings))
 print("All possible mappings: ", len(all_possible_mf_mappings))
-print("All possible mappings withough 'has_part': ", len(all_possible_mf_no_hp_mappings))
+print("All possible mappings without 'has_part': ", len(all_possible_mf_no_hp_mappings))
 print("Intersection of possible true mappings and potentially false 'has_part' mappings: ", len(all_possible_mf_mappings.intersection(potential_false_mf_hp_mappings)))
 
 print("-----biological_process-----")
