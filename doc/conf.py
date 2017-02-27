0987a9037f0dd932e9dcc227023dfb843e02ac39
Eugene Hinderer <ehinderer01@gmail.com>
2017-02-27 17:04:48 -0500

    Tried and failed to update documentation with Sphinx. Conflicts abound with Sphinx using diferent versions of the Python interpreter.

--- doc/conf.py
+++ doc/conf.py
@@ -35,8 +35,7 @@ extensions = ['sphinx.ext.autodoc',
     'sphinx.ext.doctest',
     'sphinx.ext.intersphinx',
     'sphinx.ext.mathjax',
-    'sphinx.ext.viewcode',
-    'sphinx.ext.githubpages']
+    'sphinx.ext.viewcode']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
