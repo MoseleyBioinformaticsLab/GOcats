4d12b1227f3003b3f5f1b455e5a077bdc1ae3999
Eugene Hinderer <ehinderer01@gmail.com>
2017-03-22 16:24:35 -0400

    Re-added all extensions to doc/conf.py, re-ran 'make html'.

--- doc/conf.py
+++ doc/conf.py
@@ -37,7 +37,9 @@ extensions = [
     'sphinx.ext.intersphinx',
     'sphinx.ext.todo',
     'sphinx.ext.coverage',
-    'sphinx.ext.viewcode'
+    'sphinx.ext.imgmath',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.githubpages',
 ]
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
